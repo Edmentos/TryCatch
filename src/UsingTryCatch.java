696e8510d617f9f525d9fe79f6b822a97ecba53b
Edmentos <G00414989@atu.ie>
2023-11-13 16:33:04 +0000

    *

--- src/UsingTryCatch.java
+++ src/UsingTryCatch.java
@@ -1,4 +1,19 @@
-
+import java.util.Scanner;
 
 public class UsingTryCatch {
+    public static void main(string[] args){
+        try{
+            System.out.println("Please enter a ");
+        }
+
+    }
+
+
+
+
+
+
+
+
 }
+
